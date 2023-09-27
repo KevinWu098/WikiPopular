e6a063048a7ede19afa7fe242d867f3b9411c510
Kevin Wu <kevinwu098@gmail.com>
2023-09-27 16:23:37 -0700

    Minor refactor

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -1,83 +1,87 @@
 import React, { useState } from "react";
 import {
-  View,
-  Text,
-  TextInput,
-  Image,
-  FlatList,
-  TouchableOpacity,
+    View,
+    Text,
+    TextInput,
+    Image,
+    FlatList,
+    TouchableOpacity,
 } from "react-native";
 import { useRouter } from "expo-router";
 
 import styles from "./welcome.style";
 import { icons, SIZES } from "../../../constants";
 
-const jobTypes = [
-  "Top 100",
-  "Sports",
-  "Movies",
-  "Celebrities",
-  "US News",
-  "World News",
+const topSearches = [
+    "Top 100",
+    "Sports",
+    "Movies",
+    "Celebrities",
+    "US News",
+    "World News",
 ];
 
 const Welcome = ({ searchTerm, setSearchTerm, handleClick }) => {
-  const router = useRouter();
+    const router = useRouter();
 
-  const [activeJobType, setActiveJobType] = useState("Full-time");
+    const [activeJobType, setActiveJobType] = useState("Full-time");
 
-  return (
-    <View>
-      <View style={styles.container}>
-        <Text style={styles.userName}>Hey there!</Text>
-        <Text style={styles.welcomeMessage}>See what's on WikiPopular</Text>
-      </View>
+    return (
+        <View>
+            <View style={styles.container}>
+                <Text style={styles.userName}>Hey there!</Text>
+                <Text style={styles.welcomeMessage}>
+                    See what's on WikiPopular
+                </Text>
+            </View>
 
-      <View style={styles.searchContainer}>
-        <View style={styles.searchWrapper}>
-          <TextInput
-            style={styles.searchInput}
-            value={searchTerm}
-            onChangeText={(text) => setSearchTerm(text)}
-            placeholder="What are you looking for?"
-            disabled={true}
-          />
+            <View style={styles.searchContainer}>
+                <View style={styles.searchWrapper}>
+                    <TextInput
+                        style={styles.searchInput}
+                        value={searchTerm}
+                        onChangeText={(text) => setSearchTerm(text)}
+                        placeholder="What are you looking for?"
+                        disabled={true}
+                    />
+                </View>
+                <TouchableOpacity
+                    style={styles.searchBtn}
+                    onPress={handleClick}
+                    disabled={true}
+                >
+                    <Image
+                        source={icons.search}
+                        resizeMode="contain"
+                        style={styles.searchBtnImage}
+                    />
+                </TouchableOpacity>
+            </View>
+
+            <View style={styles.tabsContainer}>
+                <FlatList
+                    data={topSearches}
+                    renderItem={({ item }) => (
+                        <TouchableOpacity
+                            style={styles.tab(activeJobType, item)}
+                            onPress={() => {
+                                setActiveJobType(item);
+                                router.push(`/search/${item}`);
+                            }}
+                            disabled={true}
+                        >
+                            <Text style={styles.tabText(activeJobType, item)}>
+                                {item}
+                            </Text>
+                        </TouchableOpacity>
+                    )}
+                    keyExtractor={(item) => item}
+                    contentContainerStyle={{ columnGap: SIZES.small }}
+                    horizontal
+                />
+            </View>
         </View>
-        <TouchableOpacity
-          style={styles.searchBtn}
-          onPress={handleClick}
-          disabled={true}
-        >
-          <Image
-            source={icons.search}
-            resizeMode="contain"
-            style={styles.searchBtnImage}
-          />
-        </TouchableOpacity>
-      </View>
-
-      <View style={styles.tabsContainer}>
-        <FlatList
-          data={jobTypes}
-          renderItem={({ item }) => (
-            <TouchableOpacity
-              style={styles.tab(activeJobType, item)}
-              onPress={() => {
-                setActiveJobType(item);
-                router.push(`/search/${item}`);
-              }}
-              disabled={true}
-            >
-              <Text style={styles.tabText(activeJobType, item)}>{item}</Text>
-            </TouchableOpacity>
-          )}
-          keyExtractor={(item) => item}
-          contentContainerStyle={{ columnGap: SIZES.small }}
-          horizontal
-        />
-      </View>
-    </View>
-  );
+    );
 };
 
 export default Welcome;
